fb619392b87fb6700e2dd5b4f1649e923125ab30
Nicholas Hoffman <njhoffman1982@gmail.com>
2019-11-09 08:00:22 -0600

    (server) basic express setup

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,8 @@
+const { version } = require('../../package.json');
+
 module.exports = {
+  version,
+  env: process.env.NODE_ENV,
   server: {
     port: 3000
   }
--- server/lib/server.js
+++ server/lib/server.js
@@ -1,8 +1,35 @@
+const express = require('express');
+const router = require('express-promise-router')();
+
 const { exceptionHandler, rejectionHandler } = require('./utils/errors');
 const config = require('../config');
 
+const startServer = async app => {
+  if (config.env !== 'test') {
+    await app.listen(config.server.port);
+    console.log(`API is running on port: ${config.server.port}`);
+  }
+  return app;
+};
+
+const initRouter = (app) => {
+  router.get('/version', (req, res) => {
+    res.send({ version: config.version });
+  });
+
+  router.get('/scan', (req, res) => {
+    res.send('OK');
+  });
+
+  app.use(router);
+  return app;
+};
+
 const initServer = async () => {
-  console.log(`Initializing Server on port ${config.server.port}...`);
+  const app = express();
+  await initRouter(app);
+  await startServer(app);
+  return app;
 };
 
 process.on('uncaughtException', exceptionHandler);
