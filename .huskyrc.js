060a7bbacf307b38deb39924f1ccdc6366483537
Nicholas Hoffman <njhoffman1982@gmail.com>
2020-01-23 17:29:04 -0600

    chore: add commitlint husky hooks and configuration

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,7 @@
 const tasks = arr => arr.join(' && ');
 
 const hooks = {
+  'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   // 'pre-commit': tasks(['npm run lint', 'npm run test']),
   // 'pre-push': './.bin/prePushVersion.sh',
   // 'post-push': tasks(['npm run test', 'npm outdated']),
