59ec87eab6c59d6dbf60c9e39d029ecd27398c2f
Nicholas Hoffman <njhoffman1982@gmail.com>
2020-01-26 12:15:59 -0600

    chore: testing pre-push version githook

--- .huskyrc.js
+++ .huskyrc.js
@@ -4,7 +4,7 @@ const hooks = {
   // 'pre-commit': tasks(['npm run lint', 'npm run test']),
   // 'pre-push': './.bin/prePushVersion.sh',
   'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-  'pre-push': tasks(['npm run test']),
+  'pre-push': tasks(['npm run test', 'npm run todos', './bin/prePushVersion']),
   'post-push': tasks([
     'npm run todos',
     'npm outdated --prefix cli',
@@ -13,3 +13,26 @@ const hooks = {
 };
 
 module.exports = {hooks};
+
+// applypatch-msg
+// commit-msg
+// husky.local.sh
+// husky.sh
+// post-applypatch
+// post-checkout
+// post-commit
+// post-merge
+// post-receive
+// post-rewrite
+// post-update
+// pre-applypatch
+// pre-auto-gc
+// pre-commit
+// pre-merge-commit
+// pre-push
+// pre-rebase
+// pre-receive
+// prepare-commit-msg
+// push-to-checkout
+// sendemail-validate
+// update
