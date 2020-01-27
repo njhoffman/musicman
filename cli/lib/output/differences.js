bc3bab10da16df03c4d456afc643dda50b8e0f8f
Nicholas Hoffman <njhoffman1982@gmail.com>
2020-01-27 17:13:18 -0600

    feat: improve output for editing differences

--- cli/lib/output/differences.js
+++ cli/lib/output/differences.js
@@ -1,7 +1,35 @@
 const chalk = require('chalk');
 const diff = require('diff');
+const columnify = require('columnify');
 
 const { consoleLog } = require('../utils/logger');
+
+const columnifyDifferences = output => {
+  const { NODE_ENV } = process.env;
+
+  const options = {
+    columns: ['key', 'value'],
+    showHeaders: false,
+    maxLineWidth: 'auto',
+    truncate: true
+  };
+
+  // console.log('output', output);
+  if (NODE_ENV === 'test') {
+    return output;
+  }
+
+  const keyRegEx = /([^:]+):(.*)$/;
+  const parsedOut = output.split('\n').map(line => {
+    const [key, value] = line.match(keyRegEx).slice(1, 3);
+    return { key, value };
+  });
+
+  return columnify(parsedOut, options)
+    .split('\n')
+    .map(line => `  ${line}`)
+    .join('\n');
+};
 
 const outputDifferences = (orig, curr) => {
   const differences =
@@ -18,6 +46,16 @@ const outputDifferences = (orig, curr) => {
     // process.stderr.write(`  ${chalk[color](part.value).trim()}`);
     diffOut += `${chalk[color](part.value)}`;
   });
+
+  if (diffOut.includes('\n')) {
+    diffOut = diffOut
+      .replace(/"/g, '')
+      .split('\n')
+      .slice(1, -1)
+      .join('\n');
+    diffOut = columnifyDifferences(diffOut);
+  }
+
   return consoleLog(diffOut);
 };
 
