d1bb5134120c0ea048b83496c17bc109d3fc597a
Nicholas Hoffman <njhoffman1982@gmail.com>
2020-01-27 15:34:58 -0600

    chore: testing pre-push version githook

--- .bin/prePushVersion.sh
+++ .bin/prePushVersion.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # always patch the version number on update
-# TODO: sed out old version number and repo name
 set -e
 
 [ -n "$_CUSTOM_RESOURCES_INNER_PRE_HOOK" ] && {
+  echo " ### version patch successful"
   exit 0
 }
 
@@ -14,6 +14,5 @@ _CUSTOM_RESOURCES_INNER_PRE_HOOK=1 git push --follow-tags
 echo
 echo " ### npm version bumped and pushed by git hook "
 echo " ### outer push will be canceled, ignore error below "
-echo " "
 echo " "
 exit 1
